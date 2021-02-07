36a782d46e9149a78edd782c71a8c8c9b1e35f92
Jelson J <jelson20@outlook.com>
2021-02-07 18:49:34 +0000

    update all

--- css/js/app.js
+++ css/js/app.js
@@ -30,12 +30,13 @@ handledHTML += `
 <img src="${item.recipe.image}" alt="Recipe">
   <div class="flex-content">
   <h1 class="title">${item.recipe.label}</h1>
-  <a class="read-more" href="${item.recipe.url}" target='_blank'>Read More</a>
+
   </div>
  <p class="card-info">Calories: ${item.recipe.calories.toFixed(2)}</p>
  <p class="card-info">Diet:${item.recipe.dietLabels.lenght > 0 ? item.recipe.dietLabels.lenght : 'No Data Found'}</p>
   <p class="card-info">Health: ${item.recipe.healthLabels}</p>
-</div>
+  <a class="read-more" href="${item.recipe.url}" target='_blank'>Read More</a>
+  </div>
 `
 })
 searchR.innerHTML = handledHTML;
