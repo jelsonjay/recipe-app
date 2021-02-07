f21714e9ac5d1cfad85f56f44a0ed3aaadbe174a
Jelson J <jelson20@outlook.com>
2021-02-07 18:05:31 +0000

    made changes on javascript

--- css/js/app.js
+++ css/js/app.js
@@ -15,8 +15,27 @@ fetchAPI()
 })
 
 async function fetchAPI(){
-const url = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}&to=30`;
+const url = `https://api.edamam.com/search?q=${searchQ}&app_id=${APP_ID}&app_key=${APP_KEY}&to=30`;
 const res = await fetch(url)
 const data = await res.json()
+handleHTML(data.hits)
 console.log(data)
+}
+function handleHTML(results){
+let handledHTML = '';
+results.map(item => {
+handledHTML += `
+<div class="card">
+<img src="${item.recipe.image}" alt="Recipe">
+  <div class="flex-content">
+  <h1 class="title">${item.recipe.label}</h1>
+  <a class="read-more" href="${item.recipe.url}" target='_blank'>Read More</a>
+  </div>
+ <p class="card-info">Calories: ${item.recipe.calories.toFixed(2)}</p>
+ <p class="card-info">Diet:${item.recipe.dietLabels.lenght > 0 ? item.recipe.dietLabels.lenght : 'No Data Found'}</p>
+  <p class="card-info">Health: ${item.recipe.healthLabels}</p>
+</div>
+`
+})
+searchR.innerHTML = handledHTML;
 }
